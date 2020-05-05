8f16473063fb68c4539c97685bda91efba4eaca2
Spencer Wood <spencer.wood@me.com>
2020-05-05 17:14:34 -0500

    going to change the list component to a simple layout

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import API from '../../utils/API';
 
-class Employee extends Component {
-    state = {
-        firstName: '',
-        lastName: ''
-    };
-
-  render(){
+function List(props) {
+    const info = (props.sortby === 'firstname') ? 'first' : 'last';
       return (
         <div>
-            <Container style={{ minHeight: 80% }}>
-                <h1 className='text-center'>Search By Employee</h1>
-                <SearchForm
-                    handleFormSubmit={this.handleFormSubmit}
-                    handleInputChange={this.handleInputChange}
-                    firstName={this.state.firstName}
-                />
-                <SearchResults results={this.state.results} />
-            </Container>
+
         </div>
       );
   }  
-}
+
 
 export default Search;
